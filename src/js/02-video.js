db4d9b36c394f909753be167009eb39ee887ba68
Anastasiya <anastasyakobets@ukr.net>
2022-11-27 11:34:00 +0200

    Install packages

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+    console.log('played the video!');
+});
+
+const savedDuration = localStorage.getItem('videoplayer-current-time');
+const parsedDuration = JSON.parse(savedDuration);
+console.log(savedDuration, parsedDuration);
+
+const onTimeupdate = function (e) {
+    const videoDuration = { seconds: e.seconds };
+    localStorage.setItem(
+        'videoplayer-current-time',
+        JSON.stringify(videoDuration.seconds)
+    );
+};
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+player
+    .setCurrentTime(parsedDuration)
+    .then(function (seconds) {
+        seconds = parsedDuration;
+        console.log('the video was played');
+    })
+    .catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+
+            default:
+                // some other error occurred
+                break;
+        }
+    });
